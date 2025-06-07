4ff8631e5887285defb8360f13d84e75419af38a
Andi Muhammad Raihan Syamsu <amuhraihan2324@gmail.com>
2025-06-07 09:36:52 +0700

    update: tambahkan middleware CORS dengan pengaturan spesifik untuk domain dan metode yang diizinkan

--- index.js
+++ index.js
@@ -54,6 +54,19 @@ app.use('/api-docs', (req, res, next) => {
     next();
 }, swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
+// Middleware untuk CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://mybookapi-production.up.railway.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, accept, Origin, X-Requested-With');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    
+    if (req.method === 'OPTIONS') {
+        return res.status(204).end();
+    }
+    next();
+});
+
 // Middleware untuk autentikasi
 const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization'];
@@ -66,19 +79,6 @@ const authenticateToken = (req, res, next) => {
     req.userEmail = email;
     next();
 };
-
-// Middleware
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'accept', 'Origin', 'X-Requested-With'],
-    credentials: true,
-    preflightContinue: false,
-    optionsSuccessStatus: 204
-}));
-
-// Tambahkan middleware untuk menangani preflight requests
-app.options('*', cors());
 
 app.use(express.json());
 app.use('/covers', express.static('covers'));
